c5346fbc731e533c2bb8773a56d96b2d1f453859
Daniel <daniil.spectr@gmail.com>
2023-02-22 22:09:57 +0200

    Update Review.jsx

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import css from './Review.module.css';
+
 class Review extends Component {
   state = {
     good: 0,
@@ -7,26 +8,46 @@ class Review extends Component {
     bad: 0,
   };
 
-  clickGood = () => {
-    this.setState(prevState => {
-      return {
-        good: prevState.good + 1,
-      };
+  onClickBtn = e => {
+    const name = e.target.name;
+    this.setState(prev => {
+      return { [name]: prev[name] + 1 };
     });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    return Math.round((good / (good + neutral + bad)) * 100);
   };
 
   render() {
     const { good, neutral, bad } = this.state;
+    //  console.log(this.countPositiveFeedbackPercentage());
     return (
       <div className={css.review}>
         <h2 className={css.reviewTitle}>Please leave feedback</h2>
-        <button onClick={this.clickGood}>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+        <button name="good" onClick={this.onClickBtn}>
+          Good
+        </button>
+        <button name="neutral" onClick={this.onClickBtn}>
+          Neutral
+        </button>
+        <button name="bad" onClick={this.onClickBtn}>
+          Bad
+        </button>
         <h2 className={css.reviewTitle}>Statistics</h2>
         <p className={css.reviewStats}>Good: {good}</p>
         <p className={css.reviewStats}>Neutral: {neutral} </p>
         <p className={css.reviewStats}>Bad: {bad}</p>
+        <p className={css.reviewStats}>Total: {this.countTotalFeedback()}</p>
+        <p className={css.reviewStats}>
+          Positive feedback: {this.countPositiveFeedbackPercentage()}%
+        </p>
       </div>
     );
   }

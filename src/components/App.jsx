1903849d46d135ff60a556b8d55ee1ac52af2492
Daniel <daniil.spectr@gmail.com>
2023-02-23 12:55:37 +0200

    taks1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,54 @@
-import Review from './Review';
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Review />
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import css from './FeedbackOptions/FeedbackOptions.module.css';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Section from './Section';
+import Notification from './Notification';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onClickBtn = e => {
+    const name = e.target.name;
+    this.setState(prev => {
+      return { [name]: prev[name] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    return Math.round((good / (good + neutral + bad)) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div className={css.review}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions click={this.onClickBtn} />
+        </Section>
+        {this.countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        )}
+      </div>
+    );
+  }
+}
